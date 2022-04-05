03c1f0a5575ea3af384d5dfd6e7023040dde06a3
austin-showen <austin.showen@gmail.com>
2022-04-04 22:47:23 -0700

    Create empty playRound() function to detwermine winner of one round
    playRound() will compare the player's choice to the computer's choice
    and return the winner of a single round.

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const moves = ["Rock", "Paper", "Scissors"];
+
 function computerPlay() {
     const randomMove = moves[Math.floor(Math.random() * moves.length)];
     return randomMove;
 }
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice = playerChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+}
+
 console.log(computerPlay());
